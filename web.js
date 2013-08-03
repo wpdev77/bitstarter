26e48288eded7594ce4541ebc0c1f69a27c81b1e
wpdev77 <cp2012@gmail.com>
2013-08-03 14:02:39 -0700

    web.js calls fs.readfilesync

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var fs=require('fs');
+var buf=require('Buffer');
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var content=String(fs.readFileSync("index.html"));
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
